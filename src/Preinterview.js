6e150d9bf507e2e848d05c00f854e5e30d7a34b8
kimjaer <wofus0422@gmail.com>
2023-12-27 12:31:41 +0900

    setInterval로 precont 3초마다 돌게하기

--- src/Preinterview.js
+++ src/Preinterview.js
@@ -1,16 +1,14 @@
-import { useRef, useState } from "react";
-import React from 'react'
-
+import React, { useEffect, useRef, useState } from "react";
 const Preinterview = () => {
   const faq = useRef(null);
-  const [shownum, numupdate] = useState(null);
+  const [shownum, numupdate] = useState(0);
   const precont = [
     {
       h3: "비전공자로써 프론트앤드 훈련에서 가장 어려웠던 것은?",
       div: " 변수 선언부터 함수선언/실행 같은 개념이 너무 생소했다.자바스크립트가 완전하지 않은 상태에서 /리액트를 들어가니 출발점을 잡기 너무 어려웠다"
     },
     {
-      h3: "비전공자로써 프론트앤드 훈련에서 가장 어려웠던 것은?",
+      h3: "리액트 프로젝트와 웹 프로젝트의 차이점을 이해하고 클라이언트의 요구에 효율적인 선택을 할 수 있나요?",
       div: " 변수 선언부터 함수선언/실행 같은 개념이 너무 생소했다.자바스크립트가 완전하지 않은 상태에서 /리액트를 들어가니 출발점을 잡기 너무 어려웠다"
     },
     {
@@ -22,8 +20,16 @@ const Preinterview = () => {
       div: " 변수 선언부터 함수선언/실행 같은 개념이 너무 생소했다.자바스크립트가 완전하지 않은 상태에서 /리액트를 들어가니 출발점을 잡기 너무 어려웠다"
     },
   ]
-  //문자.split('구분기호') -> array타입으로 전환 
-  //(문자를 정렬데이터로 변환하는 아주 중요한 메서드)
+
+  useEffect(() => {
+    const autoshow = setInterval(() => {
+      numupdate((shownum + 1) % precont.length);
+    }, 3000);
+    return () => {
+      clearInterval(autoshow);
+    }
+  }, [shownum])
+
   return (
     <section ref={faq}>
       <h2>면접전에 봐주세요.</h2>
@@ -32,17 +38,13 @@ const Preinterview = () => {
           precont.map((e, i) => {
             return (
               <li key={i}>
-                클릭한 h3의 {i}값을 shownum으로 변경
-                <h3 onClick={()=>{numupdate(i)}}>{e.h3}</h3>
-
-                {shownum === i && <div>{
-                  e.div.split('/').map((e, i) => {
-                    return (
-                      <React.Fragment key={i}>
-                        {e}
-                        <br />
-                      </React.Fragment>
-                    )
+                <h3 onClick={() => { numupdate(i) }}>{e.h3}</h3>
+                {shownum === i && <div style={{ backgroundColor: "#ccc" }}>{
+                  e.div.split('/').map((v, ii) => {
+                    return <React.Fragment key={ii}>
+                      {v}
+                      <br></br>
+                    </React.Fragment>
                   })
                 }</div>
                 }
